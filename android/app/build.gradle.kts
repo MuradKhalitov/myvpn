aa1e21ce713580ede00b35f5a7db176b87fdc664
Murad Khalitov <darggun@gmail.com>
2026-09-07 02:42:17 +0300

    feat: add app branding

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,4 +1,4 @@
-import java.util.Properties
+﻿import java.util.Properties
 
 plugins {
     id("com.android.application")
@@ -23,8 +23,8 @@ android {
         applicationId = "com.myvpn.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     compileOptions {
