613b3d3d6492dd1d4318a438d62d09c69b86c78c
Murad Khalitov <darggun@gmail.com>
2026-09-14 10:43:46 +0300

    Release Android 1.0.13

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.myvpn.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.0.12"
+        versionCode = 14
+        versionName = "1.0.13"
     }
 
     compileOptions {
